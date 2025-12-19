bbb7268f1f69a754f632ebc828e32cc945499b43
douaa Ellouadghiri Elidrissi <Douaa.Ellouadghirielidrissi@emsi-edu.ma>
2025-12-19 10:40:58 +0100

    tp6

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
-import { NavigationContainer } from "@react-navigation/native"; 
-import AuthProvider, { AuthContext } from "./context/AuthContext"; 
-import AppDrawer from "./navigation/AppDrawer"; 
-import LoginScreen from "./screens/LoginScreen"; 
-import { useContext } from "react"; 
- 
-function RootNavigator() { 
- const { user } = useContext(AuthContext); 
- 
- return user ? <AppDrawer /> : <LoginScreen />; 
-} 
- 
-export default function App() { 
- return ( 
-   <AuthProvider> 
-     <NavigationContainer> 
-       <RootNavigator /> 
-     </NavigationContainer> 
-   </AuthProvider> 
- ); 
-} 
+import { NavigationContainer } from "@react-navigation/native";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
+import AuthProvider, { AuthContext } from "./context/AuthContext";
+import AppDrawer from "./navigation/AppDrawer";
+import LoginScreen from "./screens/LoginScreen";
+import { useContext } from "react";
+
+function RootNavigator() {
+  const { user } = useContext(AuthContext);
+
+  return user ? <AppDrawer /> : <LoginScreen />;
+}
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <AuthProvider>
+        <NavigationContainer>
+          <RootNavigator />
+        </NavigationContainer>
+      </AuthProvider>
+    </Provider>
+  );
+}
